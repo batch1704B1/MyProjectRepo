ab334c4d1bc0d059dba07c747f5613e39828f5c8
batch1704B1 <46472160+batch1704B1@users.noreply.github.com>
2019-01-08 10:08:57 +0500

    line deleted
    Data deleted from check.php

--- arrays/check.php
+++ arrays/check.php
@@ -2,9 +2,6 @@
 
 
 echo "<h2>this is testing page of Git Version Control</h2>";
-echo "<p>sdgskdhkshfkdshfkhdgkhdfkghdfghdf
-dhfkhdgkhdfglkhdfglkhdfgkdflhkgd
-sdfdsjfgkdsfgksdfgkhdgldfhgldfj</p>";
 
 
 
